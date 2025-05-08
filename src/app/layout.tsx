ee515c0aa7b7a4dc1b4f138caffdf564fe352eca
Mitchell Christ <xzmaru@gmail.com>
2025-05-07 20:54:51 -0700

    setup ngrok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from 'next'
 import { Geist } from 'next/font/google'
-import '@/styles/app.css'
+import '@/app.css'
 
 const font_sans = Geist({
-	variable: '--font-geist-sans',
 	subsets: ['latin'],
 })
 
