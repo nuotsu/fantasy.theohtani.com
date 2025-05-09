2dd55b0300001020e58a24deeb86d07d30269a1a
Mitchell Christ <xzmaru@gmail.com>
2025-05-08 18:25:54 -0700

    create Standings component

--- src/ui/SignedInData.tsx
+++ src/ui/SignedInData.tsx
@@ -1,5 +1,5 @@
-import { getLeagues, getStandings } from '@/lib/yf'
-import { cn, flatten } from '@/lib/utils'
+import { getLeagues } from '@/lib/yf'
+import Standings from './Standings'
 
 export default async function SignedInData() {
 	const leagues = await getLeagues()
@@ -10,65 +10,5 @@ export default async function SignedInData() {
 				<Standings league={league} key={league.league_key} />
 			))}
 		</>
-	)
-}
-
-async function Standings({ league }: { league: any }) {
-	const standings = await getStandings(league.league_key)
-	const teams = Object.values(standings).filter((team: any) => isNaN(team))
-
-	return (
-		<div key={league.league_key}>
-			<hgroup>
-				<h3 className="font-bold">{league.name}</h3>
-				<p className="technical text-xs">League</p>
-			</hgroup>
-
-			<table className="text-center [&_td]:px-2">
-				<tbody>
-					{teams.map((team) => {
-						const t = flatten<YF.TeamData>(team.team[0])
-						const { wins, losses, ties } =
-							team.team[2].team_standings.outcome_totals
-
-						return (
-							<tr
-								className={cn(t.is_owned_by_current_login && 'font-bold')}
-								key={t.team_key}
-							>
-								<td className="w-[2ch] text-center tabular-nums">
-									{team.team[2].team_standings.rank}
-								</td>
-
-								<td>
-									<img
-										className="size-8"
-										src={t.team_logos[0].team_logo.url}
-										width={32}
-										height={32}
-									/>
-								</td>
-
-								<td className="text-left">{t.name} </td>
-
-								<td>
-									{t.managers
-										.map((manager: any) => manager.manager.nickname)
-										.join(', ')}
-								</td>
-
-								<td className="tabular-nums">
-									{wins}-{losses}-{ties}
-								</td>
-
-								<td className="tabular-nums">
-									{team.team[2].team_standings.games_back}
-								</td>
-							</tr>
-						)
-					})}
-				</tbody>
-			</table>
-		</div>
 	)
 }
---
+++ src/ui/Standings.tsx
@@ -0,0 +1,82 @@
+import { cn, flatten } from '@/lib/utils'
+import { getStandings } from '@/lib/yf'
+
+export default async function Standings({ league }: { league: any }) {
+	const standings = await getStandings(league.league_key)
+	const teams = Object.values(standings).filter((team: any) => isNaN(team))
+
+	return (
+		<div key={league.league_key}>
+			<hgroup>
+				<h3 className="font-bold">{league.name}</h3>
+				<p className="technical text-xs">League</p>
+			</hgroup>
+
+			<div className="overflow-x-auto">
+				<table className="text-center whitespace-nowrap [&_td]:px-2">
+					<thead>
+						<tr>
+							<th>Rank</th>
+							<th colSpan={2}>Team</th>
+							<th>Manager</th>
+							<th>W-L-T</th>
+							<th>Pct</th>
+							<th>GB</th>
+							<th>Moves</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						{teams.map((team) => {
+							const t = flatten<YF.TeamData>(team.team[0])
+							const { wins, losses, ties } =
+								team.team[2].team_standings.outcome_totals
+
+							return (
+								<tr
+									className={cn(t.is_owned_by_current_login && 'font-bold')}
+									key={t.team_key}
+								>
+									<td className="w-[2ch] text-center tabular-nums">
+										{team.team[2].team_standings.rank}
+									</td>
+
+									<td className="sticky left-0 px-0! backdrop-blur-[2px]">
+										<img
+											className="size-8 max-w-[initial]"
+											src={t.team_logos[0].team_logo.url}
+											width={32}
+											height={32}
+										/>
+									</td>
+
+									<td className="text-left">{t.name} </td>
+
+									<td>
+										{t.managers
+											.map((manager: any) => manager.manager.nickname)
+											.join(', ')}
+									</td>
+
+									<td className="tabular-nums">
+										{wins}-{losses}-{ties}
+									</td>
+
+									<td className="tabular-nums">
+										{team.team[2].team_standings.outcome_totals.percentage}
+									</td>
+
+									<td className="tabular-nums">
+										{team.team[2].team_standings.games_back}
+									</td>
+
+									<td className="tabular-nums">{t.number_of_moves}</td>
+								</tr>
+							)
+						})}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	)
+}
