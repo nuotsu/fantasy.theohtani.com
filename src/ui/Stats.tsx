bc9b19e92399a63a30aebc7e7e7708abfbbce0d7
Mitchell Christ <xzmaru@gmail.com>
2025-05-08 23:20:15 -0700

    mobile style adj

--- src/ui/Stats.tsx
+++ src/ui/Stats.tsx
@@ -27,90 +27,94 @@ export default async function Stats({
 		<div>
 			<h2 className="font-bold">Week {scoreboard.week}</h2>
 
-			<table className="text-center [&_td]:px-2">
-				<thead>
-					<tr>
-						<th></th>
+			<div className="overflow-x-auto">
+				<table className="text-center whitespace-nowrap [&_td]:px-2">
+					<thead>
+						<tr>
+							<th></th>
 
-						{matchups.map((matchup: { matchup: YF.Matchup }, key) => (
-							<Fragment key={key}>
-								{getPluralItems(matchup.matchup['0'].teams).map(
-									({ team }: { team: [YF.TeamData, YF.TeamStats] }, key) => {
-										const t = flatten<YF.TeamData>(team[0])
+							{matchups.map((matchup: { matchup: YF.Matchup }, key) => (
+								<Fragment key={key}>
+									{getPluralItems(matchup.matchup['0'].teams).map(
+										({ team }: { team: [YF.TeamData, YF.TeamStats] }, key) => {
+											const t = flatten<YF.TeamData>(team[0])
 
-										return (
-											<th className="relative" key={t.team_key}>
+											return (
+												<th className="relative" key={t.team_key}>
+													{key === 1 && (
+														<span className="absolute top-1/2 left-0 inline-block -translate-1/2 bg-red-500 px-1 py-0.5 text-[xx-small]/none text-white">
+															VS
+														</span>
+													)}
+
+													<TeamLogo className="mx-auto size-8" team={team[0]} />
+												</th>
+											)
+										},
+									)}
+								</Fragment>
+							))}
+						</tr>
+					</thead>
+
+					<tbody>
+						<tr>
+							<th></th>
+
+							{matchups.map((matchup: { matchup: YF.Matchup }, key) => (
+								<Fragment key={key}>
+									{getPluralItems(matchup.matchup['0'].teams).map(
+										({ team }: { team: [YF.TeamData, YF.TeamStats] }, key) => (
+											<td className="relative" key={key}>
 												{key === 1 && (
-													<span className="absolute bottom-0 left-0 inline-block -translate-x-1/2 bg-red-500 px-1 py-0.5 text-[xx-small]/none text-white">
-														VS
+													<span className="absolute left-0 -translate-x-1/2">
+														-
 													</span>
 												)}
 
-												<TeamLogo className="mx-auto size-8" team={team[0]} />
-											</th>
-										)
-									},
-								)}
-							</Fragment>
-						))}
-					</tr>
-				</thead>
+												<strong>{team[1].team_points.total}</strong>
+											</td>
+										),
+									)}
+								</Fragment>
+							))}
+						</tr>
 
-				<tbody>
-					<tr>
-						<th></th>
+						{stat_ids.map((stat_id) => {
+							const stat = stat_categories.find(
+								(category) => category.stat_id === Number(stat_id),
+							)
 
-						{matchups.map((matchup: { matchup: YF.Matchup }, key) => (
-							<Fragment key={key}>
-								{getPluralItems(matchup.matchup['0'].teams).map(
-									({ team }: { team: [YF.TeamData, YF.TeamStats] }, key) => (
-										<td className="relative" key={key}>
-											{key === 1 && (
-												<span className="absolute left-0 -translate-x-1/2">
-													-
-												</span>
+							return (
+								<tr className="tabular-nums" key={stat_id}>
+									<th className="sticky left-0 backdrop-blur-xs">
+										{stat?.display_name}
+									</th>
+
+									{matchups.map((matchup: { matchup: YF.Matchup }, key) => (
+										<Fragment key={key}>
+											{getPluralItems(matchup.matchup['0'].teams).map(
+												(
+													{ team }: { team: [YF.TeamData, YF.TeamStats] },
+													key,
+												) => (
+													<td key={key}>
+														{
+															team[1].team_stats.stats.find(
+																(stat) => stat.stat.stat_id === stat_id,
+															)?.stat.value
+														}
+													</td>
+												),
 											)}
-
-											<strong>{team[1].team_points.total}</strong>
-										</td>
-									),
-								)}
-							</Fragment>
-						))}
-					</tr>
-
-					{stat_ids.map((stat_id) => {
-						const stat = stat_categories.find(
-							(category) => category.stat_id === Number(stat_id),
-						)
-
-						return (
-							<tr className="tabular-nums" key={stat_id}>
-								<th>{stat?.display_name}</th>
-
-								{matchups.map((matchup: { matchup: YF.Matchup }, key) => (
-									<Fragment key={key}>
-										{getPluralItems(matchup.matchup['0'].teams).map(
-											(
-												{ team }: { team: [YF.TeamData, YF.TeamStats] },
-												key,
-											) => (
-												<td key={key}>
-													{
-														team[1].team_stats.stats.find(
-															(stat) => stat.stat.stat_id === stat_id,
-														)?.stat.value
-													}
-												</td>
-											),
-										)}
-									</Fragment>
-								))}
-							</tr>
-						)
-					})}
-				</tbody>
-			</table>
+										</Fragment>
+									))}
+								</tr>
+							)
+						})}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	)
 }
