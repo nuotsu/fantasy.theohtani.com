f563109425c2ed41d964536be2e447a9000ae43d
Mitchell Christ <xzmaru@gmail.com>
2025-06-15 11:27:23 -0700

    use experimental https

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,5 @@
 export const dev = process.env.NODE_ENV !== 'production'
 
 export const BASE_URL = dev
-	? // ? 'https://picked-condor-severely.ngrok-free.app'
-		'https://fantasy-theohtani.loca.lt'
+	? 'https://localhost:3000'
 	: 'https://fantasy.theohtani.com'
